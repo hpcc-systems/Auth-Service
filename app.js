c6ec96c5f15e4d88f6b395d90773f4e47c879339
Jerry Jacob <jerry.jacob@lexisnexis.com>
2020-05-19 08:48:48 -0400

    added auth middleware to / which was mistakenly removed earlier

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/users', withAuth, usersRouter);
 app.use('/auth', authRouter);
 app.use('/admin', withAuth, uiRouter);
 app.use('/', withAuth, indexRouter);
-app.use('/application', withAuth, appRouter);
+app.use('/application', withAuth, appRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
