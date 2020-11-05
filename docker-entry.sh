91520e4667fa3c17a3dba8725d6bf13d126cae8e
Jerry Jacob <jerry.jacob@lexisnexis.com>
2020-11-05 15:27:42 -0500

    nginx, docker changes

--- docker-entry.sh
+++ docker-entry.sh
@@ -4,9 +4,13 @@ host="$1"
 port="$2"
 
 until $(nc -z $host $port); do
+	echo "waiting for $host on $port..."
   sleep 2
 done
 
+echo "db migrate"
 sequelize db:migrate
+echo "db seed"
 sequelize db:seed:all
+echo "pm2 start"
 pm2-runtime start process.yml
