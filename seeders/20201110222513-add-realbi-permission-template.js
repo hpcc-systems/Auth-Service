9db1ad934693bff90517fb155de8544e26d7d6de
alfjjacob <jerry.jacob@lexisnexisrisk.com>
2021-02-03 14:39:11 -0500

    RealBI seed data

--- seeders/20201110222513-add-realbi-permission-template.js
+++ seeders/20201110222513-add-realbi-permission-template.js
@@ -6,7 +6,49 @@ module.exports = {
     return queryInterface.bulkInsert('PermissionTemplate', [{
       "id": uuidv4(),    
       "applicationType": "RealBI",
-      "permissions": JSON.stringify([]),
+      "permissions": JSON.stringify([{
+        "Dashboard": [
+          {
+            "name": "Add Dashboard",
+            "description": "Add a Dashboard",
+            "ui_values": [{
+              "value": "Deny",
+              "description": ""
+            },{
+              "value": "add",
+              "description": "h"
+            }],
+            "key": "AddDashboard",
+            "field_type": "radio"
+          },
+          {
+            "name": "Edit Dashboard",
+            "description": "Edit a dashboard in RealBI",
+            "ui_values": [{
+              "value": "Deny",
+              "description": ""
+            },{
+              "value": "edit",
+              "description": ""
+            }],
+            "key": "EditDashboard",
+            "field_type": "radio"
+          },
+          {
+            "name": "Delete Dashboard",
+            "description": "Delete a dashboard in RealBI",
+            "ui_values": [{
+              "value": "Deny",
+              "description": ""
+            },{
+              "value": "delete",
+              "description": "Can delete dashboard"
+            }],
+            "key": "DeleteDashboard",
+            "field_type": "radio"
+          }
+        ]
+      }]),
       createdAt : new Date(),
       updatedAt : new Date()
     }])
