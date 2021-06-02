32151c8d81ef3d7c4e9493cb629134e7c103bc8b
yadhap Dahal <yadhap.dahal@lexisnexisrisk.com>
2021-06-02 17:44:03 -0400

    added seeder for default app tombolo

--- seeders/20210528134336-tombolo-role-default.js
+++ seeders/20210528134336-tombolo-role-default.js
@@ -1,46 +1,47 @@
-'use strict';
-const uuidv4  = require('uuid/v4');
-
+"use strict";
+const uuidv4 = require("uuid/v4");
 
 module.exports = {
-  up:  (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('roles', [{
-      id: uuidv4(),
-      applicationType : 'Tombolo',
-      name: 'Tombolo_Admin',
-      managedBy: 'LNRS',
-      CreatedAt: new Date(),
-      updatedAt: new Date(),
-      permissions :JSON.stringify({
-        "AddFiles": "add",
-        "EditFiles": "edit",
-        "AddDataflow": "add", 
-        "DeleteFiles": "delete", 
-        "EditDataflow": "edit", 
-        "DeleteDataflow": "delete", 
-        "ReadDataflowInstance": "add"
-      })},
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("roles", [
       {
         id: uuidv4(),
-        applicationType : 'Tombolo',
-        name: 'Tombolo_Creator',
-        managedBy: 'LNRS',
+        applicationType: "Tombolo",
+        name: "Tombolo_Admin",
+        managedBy: "LNRS",
         CreatedAt: new Date(),
         updatedAt: new Date(),
-        permissions : JSON.stringify({
-          "AddFiles": "add",
-          "EditFiles": "edit",
-          "AddDataflow": "add", 
-          "DeleteFiles": "delete", 
-          "EditDataflow": "edit", 
-          "DeleteDataflow": "delete", 
-          "ReadDataflowInstance": "add"
-        })}
-    ]) 
+        permissions: JSON.stringify({
+          AddFiles: "add",
+          EditFiles: "edit",
+          AddDataflow: "add",
+          DeleteFiles: "delete",
+          EditDataflow: "edit",
+          DeleteDataflow: "delete",
+          ReadDataflowInstance: "add",
+        }),
+      },
+      {
+        id: uuidv4(),
+        applicationType: "Tombolo",
+        name: "Tombolo_Creator",
+        managedBy: "LNRS",
+        CreatedAt: new Date(),
+        updatedAt: new Date(),
+        permissions: JSON.stringify({
+          AddFiles: "add",
+          EditFiles: "edit",
+          AddDataflow: "add",
+          DeleteFiles: "delete",
+          EditDataflow: "edit",
+          DeleteDataflow: "delete",
+          ReadDataflowInstance: "add",
+        }),
+      },
+    ]);
   },
 
-  down:  (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('roles', null, {});
-  }
- 
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("roles", null, {});
+  },
 };
---
+++ seeders/20210602205126-default-application.js
@@ -0,0 +1,24 @@
+"use strict";
+const uuidv4 = require("uuid/v4");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("application", [
+      {
+        id: uuidv4(),
+        name: "Tombolo",
+        description: "Add Description Here",
+        email: " your email here",
+        owner: "Owner Name",
+        clientId: "Client ID",
+        CreatedAt: new Date(),
+        updatedAt: new Date(),
+        applicationType: "Tombolo",
+      },
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("roles", null, {});
+  },
+};
